3713f41b205211d8be8b14b227e47a6535eb579b
Karen Xie <karen.xie@myob.com>
2018-08-07 08:01:39 +1200

    unit test project

--- TicTacToe_Vim/Program.cs
+++ TicTacToe_Vim/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicTacToe_Vim
+﻿namespace TicTacToe_Vim
 {
     class Program
     {
---
+++ TicTactToe_VimTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTactToe_VimTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
