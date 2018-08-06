d15df1d2516ccc0b84b817b38e6306d833eaba60
Karen Xie <karen.xie@myob.com>
2018-08-07 08:15:00 +1200

    add first test

---
+++ TicTacToe_Vim/TicTacToe.cs
@@ -0,0 +1,12 @@
+﻿namespace TicTacToe_Vim
+{
+    public class TicTacToe
+    {
+        public string RenderBoard()
+        {
+            return "...\n" +
+                   "...\n" +
+                   "...\n";
+        }
+    }
+}
--- TicTactToe_VimTests/UnitTest1.cs
+++ TicTactToe_VimTests/UnitTest1.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe_Vim;
 
 namespace TicTactToe_VimTests
 {
     [TestClass]
-    public class UnitTest1
+    public class TicTacToeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldRenderAnEmpty3X3Board()
         {
+            const string expected = "...\n" +
+                                    "...\n" +
+                                    "...\n";
+            var game = new TicTacToe();
+
+            var result = game.RenderBoard();
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
