c1a8e167903292793737b7f0cf127e7488c4c5fb
Karen Xie <karen.xie@myob.com>
2018-08-07 08:39:28 +1200

    new render function takes in a list

--- TicTacToe_Vim/TicTacToe.cs
+++ TicTacToe_Vim/TicTacToe.cs
@@ -1,4 +1,7 @@
-﻿namespace TicTacToe_Vim
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToe_Vim
 {
     public class TicTacToe
     {
@@ -7,6 +10,22 @@
             return "...\n" +
                    "...\n" +
                    "...\n";
+        }
+
+        public object RenderBoard(List<string> board)
+        {
+            var outputBoard = "";
+
+            for (var position = 0; position < board.Count; position++)
+            {
+                outputBoard += board[position];
+                if (position % 3 == 2)
+                {
+                    outputBoard += "\n";
+                }
+            }
+
+            return outputBoard;
         }
     }
 }
---
+++ TicTactToe_VimTests/TicTacToeTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe_Vim;
+
+namespace TicTactToe_VimTests
+{
+    [TestClass]
+    public class TicTacToeTests
+    {
+        [TestMethod]
+        public void ShouldRenderAnEmpty3X3Board()
+        {
+            const string expected = "...\n" +
+                                    "...\n" +
+                                    "...\n";
+            var input = new List<string>
+            {
+                ".", ".", ".",
+                ".", ".", ".",
+                ".", ".", ".",
+            };
+            var game = new TicTacToe();
+
+            var result = game.RenderBoard(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ShouldRenderABoardWithOneX()
+        {
+            const string expected = "...\n" +
+                                    ".X.\n" +
+                                    "...\n";
+            var input = new List<string>
+            {
+               ".", ".", ".",
+               ".", "X", ".",
+               ".", ".", ".", 
+            };
+
+            var game = new TicTacToe();
+
+            var result = game.RenderBoard(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
